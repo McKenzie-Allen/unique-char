692052cb5e237814f90529b50cfb7d4211099788
McKenzie Allen <mckenzie.allen144@gmail.com>
2021-12-14 20:51:30 -0700

    first commit

---
+++ hasUniqueChars.js
@@ -0,0 +1,17 @@
+// Write your code below
+
+
+
+// // Epic Joke
+
+// let ingredients = ["sugar", "water", "cinnamon", "cloves", "ginger", "nutmeg", "pumpkin"];
+
+// function pumpkinSpiceLatte(arr) {
+//     for (let i = 0; i < arr.length; i++) {
+//         if (ingredients[i] === "pumpkin") {
+//             arr.splice(i, 1)
+//         }
+//     }
+//     return arr
+// }
+// console.log(pumpkinSpiceLatte(ingredients))
